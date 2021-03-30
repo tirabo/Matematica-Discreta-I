2d744004c5efe5da8d69d8a53aab786632004eea
Alejandro Tiraboschi <tirabo@gmail.com>
2021-03-30 20:39:46 -0300

    criba_primos

--- 2021-1/python/criba_Eratostenes.py
+++ 2021-1/python/criba_Eratostenes.py
@@ -7,13 +7,28 @@ def criba(n: int) -> list[int]:
     for i in range(1, n + 1, 2):
         criba[i] = i # en los lugares impares se pone criba[i] = i
     i = 3
-    while i*i < n + 1:
-        if criba[i] == i:
-            for j in range(i*i, n + 1, i):
+    while i*i <= n : # se itera desde 3 a i <= sqrt(n)
+        if criba[i] == i: # i es primo
+            for j in range(i*i, n + 1, i): # j desde i^2 a sqrt(n) saltando de a i
                 if criba[j] == j:
-                    criba[j] = i
+                    criba[j] = i # si j no es divisible por un primo < i, se pone i
         i = i + 1
-    return criba # post:  criba[i] = menor factor primo de i (i >0)
+    return criba # post:  criba[i] = menor factor primo de i (i >1)
+
+def criba_primos(n: int) -> list[int]:
+    # pre: n número natural
+    # post: se obtiene ''primos'' la lista de números primos hasta n usando
+    #       la criba de Eratóstenes
+    primos = [] # lista vacía
+    tachados = [] # lista de números tachados 
+    for i in range(2, n+1):
+        if i not in tachados:
+            primos.append(i) # agregar i a primos
+            k = 2
+            while k * i <= n:
+                tachados.append(k * i) # agrega k*i a tachados
+                k = k + 1
+    return primos
 
 def primos_hasta(n: int) -> list[int]:
     # pre: n > 0
@@ -37,8 +52,9 @@ def desc_prima(n: int) -> list[int]:
         
     return primos
 
-
+print(criba(25))
 # print(len(criba(10**8))) # esto ya toma mucho tiempo 
-# print(primos_hasta(101))
+print(primos_hasta(1024))
+print(criba_primos(1024))
 print(desc_prima(1024))
 
