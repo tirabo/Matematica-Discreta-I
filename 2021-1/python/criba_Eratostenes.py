54a1caf2688ea4cf3d1e11d3784a4379eeaa51ec
Alejandro Tiraboschi <tirabo@gmail.com>
2021-04-22 17:39:32 -0300

    C10 C11 y Tarea 4

--- 2021-1/python/criba_Eratostenes.py
+++ 2021-1/python/criba_Eratostenes.py
@@ -34,7 +34,7 @@ def primos_hasta(n: int) -> list[int]:
     # pre: n > 0
     # post: devuelve primos, una lista de primos  <= n
     criba_n = criba(n)
-    primos = []
+    primos = [2]
     for i in range(3, n + 1, 2):
         if criba_n[i] == i:
             primos.append(i)
@@ -49,12 +49,42 @@ def desc_prima(n: int) -> list[int]:
         p = criba(k)[-1] # el menor primo que divide a k
         primos.append(p)
         k = k // p
-        
     return primos
 
-print(criba(25))
-# print(len(criba(10**8))) # esto ya toma mucho tiempo 
-print(len(primos_hasta(1024000)))
-print(len(criba_primos(1024000)))
-# print(desc_prima(1024))
+def criba_clasica(n: int) -> list[int]:
+    # pre: n > 0
+    # post: devuelve la lista de primos <= n usando el método de la criba de Eratóstenes
+    primos = []
+    todos = list(range(n+1))
+    todos[0], todos[1] = 0, 0
+    j = 2
+    while  j <= n:
+        if todos[j] != 0:
+            primos.append(j)
+            k = 1
+            while j + j*k <= n:
+                todos[j + j*k] = 0
+                k = k + 1
+        j = j + 1
+    return primos
 
+
+
+
+def main():
+    # print(criba(25))
+    # print(len(criba(10**8))) # esto ya toma mucho tiempo 
+    # print(len(primos_hasta(102400)))
+    # print(len(criba_primos(102400)))
+    # print(desc_prima(1024))
+    n = 997
+    print(len(primos_hasta(n))) # Eficiente
+    print(primos_hasta(n))
+    # print(len(criba_primos(n))) # No eficiente
+    print('')
+    print(len(criba_clasica(n))) # Eficiente, consume memoria (arreglo de longitud n)
+    print(criba_clasica(n))
+
+
+if __name__ == "__main__":
+    main()
