c81e2e570a1ad053255ad9dd421ce3f5cfedc6e3
Alejandro Tiraboschi <tirabo@gmail.com>
2023-04-01 18:45:17 -0300

    menoactualizamos cantero

--- python/cantero.py
+++ python/cantero.py
@@ -22,31 +22,32 @@ Se quieren pintar los vértices de un polígono de 6 lados con 4 colores sin que
 Dos casos:
 1) las rotaciones no son equivalentes;
 2) las rotaciones son equivalentes. 
-Solución (fuerza bruta): 1) 730; 2) 130. 
+Solución (fuerza bruta): 1) 732; 2) 130. 
 """
 
 
 
-def acomodar_plantines(cantero = 6, cls_plantines= 4):
-    # cantero : cantidad de plantines en el cantero
-    # cls_plantines : cantidad de clases de plantines
+def pintar_poligono_fb(ver = 6, col = 4):
+    # ver : entero >=2 
+    # col : cantidad de colores >= 2
+    # post: devuelve la cantidad de formas de pintar los vértices de un polígono de ver lados con col colores sin que haya dos vértices consecutivos pintados del mismo color. El par que devuelve es en la primera coordenada sin equivalencia de rotaciones y la segunda con equivalencia con rotaciones.
     # solución fuerza bruta: se calculan todas las posibilidades y se eliminan las que no sirven 
-    cls = list(range(cls_plantines))
-    cls_repetidos = [cls]*cantero
+    cls = list(range(col))
+    cls_repetidos = [cls]*ver
     plt1 = list(product(*cls_repetidos))
     r0 = [list(w) for w in plt1]
-    # r0 son todos los canteros posibles, sin restricciones, sin equivalencia de rotaciones (es cls**cantero)
+    # r0 son todos los coloreos posibles, sin restricciones, sin equivalencia de rotaciones (es cls**n)
     r1 = []
     for u in r0:
         sirve = True
-        for i in range(cantero - 1):
+        for i in range(ver - 1):
             if u[i] == u[i + 1]:
                 sirve = False
-        if u[cantero - 1] == u[0]:
+        if u[ver - 1] == u[0]:
             sirve = False
         if sirve:
             r1.append(u)
-    # r1 son todos los canteros circulares posibles donde no puede haber dos clases iguales contiguas.
+    # r1 son todos los coloreos posibles donde no puede haber dos colores iguales contiguos.
     # No hay equivalencia por rotaciones
     r2 = []
     raux = []
@@ -55,53 +56,44 @@ def acomodar_plantines(cantero = 6, cls_plantines= 4):
         if u in raux:
             sirve = False
         else:
-            for i in range(cantero):  # agrega todas las rotaciones de u a raux
-                x = []
-                for j in range(cantero):
-                    x.append(u[(i + j) % cantero])
-                if x not in raux:
-                    raux.append(x)
+            for i in range(ver):  # agrega todas las rotaciones de u a raux
+                raux.append(u[i:] + u[:i])
         if sirve:
             r2.append(u)
     # r2 son todos los canteros circulares posibles donde no puede haber dos clases iguales contiguas.
-    # Hay equivalencia por rotaciones
-    # for u in r2:
-    #     print u
     return len(r1), len(r2)
-
-# cantero = 4
-# for cls_plantines in range(1, cantero + 1 ):
-#     print('vértices:', cantero, 'colores:',cls_plantines,acomodar_plantines(cantero, cls_plantines))
 
 
 # A partir de aquí como se resuelve 1). 
 
-def pintar_linea_rec(c, n, i):
-    # pre: n cantidad de puntos de la linea >= 2, c cantidad de colores, 0 <= i < c
-    # post:  devuelve la cantidad de formas de pintar la linea de n puntos con 
-    #       c colores tal que:
+
+def pintar_linea_rec(ver, i, col):
+    # pre: ver cantidad de puntos de la linea >= 2, col cantidad de colores, 0 <= i < col
+    # post:  devuelve la cantidad de formas de pintar la linea de ver puntos con 
+    #       col colores tal que:
     #       1) comienza con color 0
     #       2) no hay dos puntos consecutivos pintados del mismo color
     #       3) termina en el color i
 
     # Se  hará recursión sobre n >= 2
     ret = 0
-    if n == 2:
+    if ver == 2:
         if i == 0:
             ret = 0
         else:
             ret = 1
     else:
         if i == 0:
-            ret = 3 * pintar_linea(c, n - 1, 1)
+            ret = (col - 1) * pintar_linea_rec(ver - 1, 1, col)
         else:
-            ret = pintar_linea(c, n - 1, 0) + 2 * pintar_linea(c, n - 1, 1)
+            ret = pintar_linea_rec(ver - 1, 0, col) + (col - 2) * pintar_linea_rec(ver - 1, 1, col)
     return ret
 
-def pintar_linea_pd(c, n, i):
-    # pre: n cantidad de puntos de la linea >= 2, c cantidad de colores, 0 <= i < c
+
+def pintar_linea_pd(ver, i, col):
+    # pre: col cantidad de colores, ver cantidad de puntos de la linea >= 2, 0 <= i < c
     # post:  devuelve la cantidad de formas de pintar la linea de n puntos con 
-    #       c colores tal que:
+    #       col colores tal que: 
     #       1) comienza con color 0
     #       2) no hay dos puntos consecutivos pintados del mismo color
     #       3) termina en el color i
@@ -110,20 +102,20 @@ def pintar_linea_pd(c, n, i):
     ret = 0
     
     # Primera forma: ocupa más espacio pero es más fácil de entender
-    # matriz = [[0, 0]] * n
+    # matriz = [[0, 0]] * ver
     # matriz[1] = [0, 1]
-    # for k in range(2, n):
-    #     matriz[k] = [3 * matriz[k - 1][1], matriz[k - 1][0] + 2 * matriz[k - 1][1]]
+    # for k in range(2, ver):
+    #     matriz[k] = [(col -1) * matriz[k - 1][1], matriz[k - 1][0] + (col -2) * matriz[k - 1][1]]
     # if i == 0:
-    #     ret = matriz[n - 1][0]
+    #     ret = matriz[ver - 1][0]
     # else:
-    #     ret = matriz[n - 1][1]
+    #     ret = matriz[ver - 1][1]
     # return ret
     
     # Segunda forma: mínimo espacio, mínimo procesamiento
     mpd = [[0, 1], [0,0]] 
-    for k in range(2, n):
-        mpd[1] = [3 * mpd[0][1], mpd[0][0] + 2 * mpd[0][1]]
+    for k in range(2, ver):
+        mpd[1] = [(col -1) * mpd[0][1], mpd[0][0] + (col - 2) * mpd[0][1]]
         mpd[0] = mpd[1]
     if i == 0:
         ret = mpd[1][0]
@@ -132,22 +124,93 @@ def pintar_linea_pd(c, n, i):
     return ret
 
 
-def acomodar_plantines_2(n = 6, c = 4):
-    # n : cantidad de plantines en el cantero
-    # c : cantidad de clases de plantines
+def pintar_poligono_rec(ver = 6, col = 4):
+    # ver : cantidad de plantines en el cantero
+    # col : cantidad de clases de plantines
     # solución recursiva: se calcula la cantidad de formas de pintar una linea 
-    #     de n + 1  puntos con c colores tal que comienza  y  termina en 0 
+    #     de ver + 1  puntos con col colores tal que comienza  y  termina en 0 
     #     y no hay dos puntos consecutivos pintados del mismo color,
-    # ret = pintar_linea_rec(c, n + 1, 0) # recursivo
-    ret = pintar_linea_pd(c, n + 1, 0) # programación dinámica
-    # Identificando el primer y último vértice se resuelve el problema de los n plantines cuando el primer vértice es 0. Ha que multiplicar por la cantidad de colores para no tener restricciones en el primer vértice.
-    return ret * c
+    ret = pintar_linea_rec(ver + 1, 0, col) # recursivo
+    # Identificando el primer y último vértice se resuelve el problema de los n plantines cuando el primer vértice es 0. 
+    # 
+    # Hay que multiplicar por la cantidad de colores para no tener restricciones en el primer vértice.
+    return ret * col
+
+def pintar_poligono_pd(ver = 6, col = 4):
+    # ver : cantidad de plantines en el cantero
+    # col : cantidad de clases de plantines
+    ret = pintar_linea_pd(ver + 1, 0, col) # programación dinámica
+    # Identificando el primer y último vértice se resuelve el problema de los polígonos de ver lados cuando el primer vértice es 0. 
+    # 
+    # Hay que multiplicar por la cantidad de colores para no tener restricciones en el primer vértice.
+    return ret * col
 
 
-ver, col = 6, 4
-print('vértices:', ver, 'colores:', col,'\n' ,acomodar_plantines(ver, col)[0])
+def pintar_linea_casos(ver, i, col):
+    # pre: col cantidad de colores >= 2, ver cantidad de puntos de la linea >= 2, 
+    #       0 <= i < col
+    # post:  devuelve las formas de pintar la linea de ver puntos con 
+    #       col colores tal que: 
+    #       1) comienza con color 0
+    #       2) no hay dos puntos consecutivos pintados del mismo color
+    #       3) termina en el color i
+    #       Lo  que devuelve es una lista de listas, donde cada lista es una 
+    #       forma de pintar la linea (cada lista empieza en 0 y termina en i)
 
-ver, col = 6, 4
-print('vértices:', ver, 'colores:', col,'\n' ,acomodar_plantines_2(ver, col))
+    mpd, ret = [], []
+    for j in range(1, col):  # todos los casos de longitud 2
+        mpd.append ([0, j])
+
+    for k in range(2, ver):
+        aux_casos = []
+        for caso in mpd:
+            for c in range(col):
+                if caso[-1] != c:
+                    aux_casos.append(caso + [c])
+        mpd = aux_casos
+    # mpd ahora son todos los casos de longitud ver pueden terminar en caulquier vértice
+    # hay que filtrar los que terminan en i
+    for caso in mpd:
+        if caso[-1] == i:
+            ret.append(caso)
+    return ret
+
+
+def pintar_poligono_formas(ver = 6, col = 4):
+    # pre: ver, col >= 2
+    # post: devuelve todas las posibles formas de pintar un polígono de ver lados 
+    #       con col colores sin que haya dos vértices consecutivos pintados 
+    #       del mismo color.
+    # 
+    # solución recursiva: se calcula la cantidad de formas de pintar una linea 
+    #     de ver + 1  puntos con col colores tal que comienza  y  termina en 0 
+    #     y no hay dos puntos consecutivos pintados del mismo color,
+    ret = pintar_linea_casos(ver + 1, 0, col) # programación dinámica
+    # Identificando el primer y último vértice se resuelve el problema de los polígonos de ver lados cuando el primer vértice es 0. 
+    # 
+    # Hay que multiplicar por la cantidad de colores para no tener restricciones en el primer vértice.
+    return ret * col
+
+def main():
+    ver, col = 6, 4
+    print('Fuerza bruta. Vértices:', ver, 'colores:',col,'->',pintar_poligono_fb(ver, col))
+    # Fuerza bruta no funciona para ver o col grandes, por ejemplo para ver, col = 10, 4 se demora varios minutos en calcular.
+
+    ver, col = 20, 4
+    print('Recursión. Vértices:', ver, 'colores:', col,'->' , pintar_poligono_rec(ver, col))
+    # Recursión se demora mucho para números grandes. Por ejemplo para ver, col = 50, 4 se demora varios minutos en calcular.
+
+    ver, col = 1000, 80
+    print('Progamación dinámica. Vértices:', ver, 'colores:', col,'->' , pintar_poligono_pd(ver, col))
+    # Programación dinámica funciona para ver o col grandes
+
+    ver, col = 10, 4
+    print('Formas. Vértices:', ver, 'colores:', col,'->' , len(pintar_poligono_formas(ver, col)))
+    # Esto se complica para ver grandes (guarda en memoria todas las formas). Por ejemplo para ver, col = 20, 4 se demora varios minutos en calcular.
+
+
+if __name__ == "__main__":
+    main()
+
 
 
